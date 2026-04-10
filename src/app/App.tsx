cd87cbd7223aaf59fc7973afeee4d73cbd9b221d
wlsldm <lavesujin@gmail.com>
2026-04-10 11:29:58 +0900

    chore: 초기 세팅

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import "./index.css"
+import { QueryProviders } from "./providers/QueryProviders"
+
+function App() {
+  return (
+    <QueryProviders>
+      <div>app</div>
+    </QueryProviders>
+  )
+}
+
+export default App
