1a4b9323473ce6f2e230c0ca7ec68f0c12b44863
wlsldm <lavesujin@gmail.com>
2026-05-07 00:23:42 +0900

    fix: 누락된 이미지 요소 추가

--- src/shared/ui/PhotoRadio/PhotoRadio.tsx
+++ src/shared/ui/PhotoRadio/PhotoRadio.tsx
@@ -3,15 +3,18 @@ import { Radio } from '@/shared/ui/Radio/Radio';
 import type { InputHTMLAttributes } from 'react';
 
 type PhotoRadioProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'type'> & {
+  src: string;
+  alt?: string;
   className?: string;
 };
 
-export function PhotoRadio({ className, ...radioProps }: PhotoRadioProps) {
+export function PhotoRadio({ src, alt = '', className, ...radioProps }: PhotoRadioProps) {
   return (
     <label
       aria-label="사진 선택"
       className={cn('relative flex cursor-pointer overflow-hidden rounded-xs', className)}
     >
+      <img src={src} alt={alt} className="h-[115px] w-[115px] object-cover" />
       <Radio className="absolute top-1 right-1" {...radioProps} />
     </label>
   );
