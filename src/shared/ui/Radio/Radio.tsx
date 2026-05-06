5322afb0fbdb1ca103fcbc7804be6b67db2d2ced
wlsldm <lavesujin@gmail.com>
2026-05-07 00:38:18 +0900

    style: 코드 포맷 정리

--- src/shared/ui/Radio/Radio.tsx
+++ src/shared/ui/Radio/Radio.tsx
@@ -8,13 +8,7 @@ type RadioProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'type'> & {
 export function Radio({ checked, onChange, className, ...props }: RadioProps) {
   return (
     <div className={cn('flex cursor-pointer', className)}>
-      <input
-        type="radio"
-        className="sr-only"
-        checked={checked}
-        onChange={onChange}
-        {...props}
-      />
+      <input type="radio" className="sr-only" checked={checked} onChange={onChange} {...props} />
       <div className="flex h-5 w-5 items-center justify-center rounded-full bg-gray-200">
         {checked && <div className="bg-main-001 h-[14px] w-[14px] rounded-full" />}
       </div>
