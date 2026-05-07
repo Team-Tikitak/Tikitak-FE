1bb48d1476b0f41a189a3dda0b64879ddf56160a
wlsldm <lavesujin@gmail.com>
2026-05-07 21:22:02 +0900

    feat: 멤버 카드 컴포넌트

---
+++ src/shared/ui/MemberCard/MemberCard.tsx
@@ -0,0 +1,38 @@
+import CloseIcon from '@/shared/assets/Icon/CloseIcon.svg?react';
+import { cn } from '@/shared/lib';
+import { Avatar } from '../Avatar/Avatar';
+import type { ComponentPropsWithoutRef } from 'react';
+
+type MemberCardProps = ComponentPropsWithoutRef<'div'> & {
+  avatarSrc: string;
+  name: string;
+  email: string;
+  onRemove?: () => void;
+};
+
+export const MemberCard = ({
+  avatarSrc,
+  name,
+  email,
+  onRemove,
+  className,
+  ...props
+}: MemberCardProps) => {
+  return (
+    <div
+      className={cn('flex w-full items-center justify-between gap-2 py-3 pr-4', className)}
+      {...props}
+    >
+      <div className="flex items-center gap-3">
+        <Avatar src={avatarSrc} alt={name} size="lg" />
+        <div className="flex flex-col gap-1">
+          <span className="body-9 text-black">{name}</span>
+          <span className="body-1 text-gray-700">{email}</span>
+        </div>
+      </div>
+      <button type="button" aria-label="멤버 삭제" onClick={onRemove}>
+        <CloseIcon className="size-6 cursor-pointer text-gray-700" />
+      </button>
+    </div>
+  );
+};
