bfa60c04ee15a4e0695a14dd6716b496b5925b3f
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2026-05-08 14:56:02 +0900

    feat: 앱 공통 레이아웃 컴포넌트 추가

---
+++ src/app/layout/index.ts
@@ -0,0 +1,10 @@
+import { AppLayoutBottom, AppLayoutContent, AppLayoutHeader, AppLayoutRoot } from './AppLayout';
+
+export * from './AppLayout';
+
+export const AppLayout = {
+  Root: AppLayoutRoot,
+  Header: AppLayoutHeader,
+  Content: AppLayoutContent,
+  Bottom: AppLayoutBottom,
+};
