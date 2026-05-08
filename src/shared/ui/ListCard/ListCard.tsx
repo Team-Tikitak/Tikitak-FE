cef6a44210180b6f361b277771b612f80ed3fd7a
wlsldm <lavesujin@gmail.com>
2026-05-08 16:03:21 +0900

    refactor: 아이콘 네이밍 수정

--- src/shared/ui/ListCard/ListCard.tsx
+++ src/shared/ui/ListCard/ListCard.tsx
@@ -1,6 +1,6 @@
 import { type ComponentPropsWithoutRef } from 'react';
 import { tv } from 'tailwind-variants';
-import LeftIcon from '@/shared/assets/Icon/LeftIcon.svg?react';
+import RightIcon from '@/shared/assets/Icon/RightIcon.svg?react';
 
 const ListCardVariants = tv({
   base: 'w-full h-14 gap-2 px-[18px] py-[14px] flex items-center justify-between text-gray-900 border-b border-gray-200 cursor-pointer',
@@ -14,7 +14,7 @@ export function ListCard({ title, className, ...props }: ListCardProps) {
   return (
     <button type="button" className={ListCardVariants({ className })} {...props}>
       <span className="body-7">{title}</span>
-      <LeftIcon className="size-5" />
+      <RightIcon className="size-5" />
     </button>
   );
 }
