19181408fd9668ad7a23e3f4dc2c3dcee2c33e47
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2026-05-12 19:49:44 +0900

    fix: 루트 레이아웃 overflow를 hidden으로 변경

--- src/app/layout/RootLayout.tsx
+++ src/app/layout/RootLayout.tsx
@@ -15,7 +15,7 @@ export const RootLayout = ({ className }: RootLayoutProps) => {
   return (
     <div className={cn('mx-auto flex min-h-dvh w-full max-w-[393px] flex-col bg-white', className)}>
       <Ssgoi config={ssgoiConfig}>
-        <div className="relative z-0 flex min-h-0 flex-1 flex-col overflow-x-clip">
+        <div className="relative z-0 flex min-h-0 flex-1 flex-col overflow-x-hidden">
           <Outlet />
         </div>
       </Ssgoi>
