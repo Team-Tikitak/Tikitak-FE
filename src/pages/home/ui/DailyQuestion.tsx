ef68c77df810fede8109d37a8bcf0635a21c359f
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2026-05-13 22:07:05 +0900

    refactor: TodayQuestion을 DailyQuestion으로 변경

--- src/pages/home/ui/DailyQuestion.tsx
+++ src/pages/home/ui/DailyQuestion.tsx
@@ -2,22 +2,22 @@ import TakSpark from '@/shared/assets/Character/TakSpark.svg';
 import FrontIcon from '@/shared/assets/Icon/FrontIcon.svg?react';
 import { ImageWithFallback } from '@/shared/ui';
 
-interface TodayQuestionProps {
+interface DailyQuestionProps {
   question: string;
   imageUrls: string[];
   onClick?: () => void;
 }
 
-export const TodayQuestion = ({ question, imageUrls, onClick }: TodayQuestionProps) => {
+export const DailyQuestion = ({ question, imageUrls, onClick }: DailyQuestionProps) => {
   return (
     <button
       type="button"
-      className="bg-main-001 flex w-full items-center justify-between rounded-md p-4 text-white"
+      className="bg-main-001/70 flex w-full items-center justify-between rounded-2xl p-4 text-white shadow-[inset_0_0_3.267px_0_rgba(255,255,255,0.3),inset_0_0_5.833px_0_rgba(255,255,255,0.1)] backdrop-blur-sm"
       onClick={onClick}
     >
-      <div className="flex flex-col">
-        <span className="logo text-left">Today's Tiki-tak!</span>
-        <span className="body-7 mt-[2px] text-left">{question}</span>
+      <div className="flex flex-col gap-[2px]">
+        <span className="logo text-left">Today’s Tiki-tak!</span>
+        <span className="body-7 text-left">{question}</span>
         <div className="mt-2 flex items-center -space-x-1">
           {imageUrls.map((url) => (
             <ImageWithFallback key={url} src={url} fallbackSrc={TakSpark} />
