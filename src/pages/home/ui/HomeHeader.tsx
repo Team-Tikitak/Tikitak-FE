708fd8144d0b1b04e3d08b0439eefa666d166e75
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2026-05-13 22:06:26 +0900

    fix: 홈 헤더 디자인 정합 및 높이 상수 노출

--- src/pages/home/ui/HomeHeader.tsx
+++ src/pages/home/ui/HomeHeader.tsx
@@ -2,6 +2,8 @@ import BellIcon from '@/shared/assets/Icon/BellIcon.svg?react';
 import ChevronDownIcon from '@/shared/assets/Icon/ChevronDownIcon.svg?react';
 import TikiTakLogo from '@/shared/assets/Logo/tiki-tak_Logo.svg?react';
 
+export const HOME_HEADER_HEIGHT = 72;
+
 interface HomeHeaderProps {
   teamName: string;
   onTeamSelect?: () => void;
@@ -9,12 +11,12 @@ interface HomeHeaderProps {
 
 export const HomeHeader = ({ teamName, onTeamSelect }: HomeHeaderProps) => {
   return (
-    <header className="flex h-21 w-full justify-between bg-white px-5 pt-2">
+    <header className="flex h-[72px] w-full items-start justify-between bg-white px-5 pt-2 pb-[3px]">
       <div className="flex flex-col gap-2">
         <TikiTakLogo className="h-[29px] w-[76px]" />
         <button
           type="button"
-          className="body-4 flex items-center gap-3 text-black"
+          className="body-9 flex items-center gap-1 text-black"
           onClick={onTeamSelect}
         >
           {teamName}
