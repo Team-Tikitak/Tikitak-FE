c2618330623a30ce5f32c415755d4b5a3895e80c
wlsldm <lavesujin@gmail.com>
2026-05-13 11:47:25 +0900

    style: 코드 포맷 정리

--- src/pages/home/model/mock.ts
+++ src/pages/home/model/mock.ts
@@ -1,9 +1,34 @@
 import { type Pin } from './types';
 
 export const MOCK_PINS: Pin[] = [
-  { latitude: 37.5507563, longitude: 126.9254901, feedCount: 3, thumbnailImageUrl: 'https://picsum.photos/seed/1/87' },
-  { latitude: 37.552, longitude: 126.927, feedCount: 1, thumbnailImageUrl: 'https://picsum.photos/seed/2/87' },
-  { latitude: 37.5495, longitude: 126.924, feedCount: 5, thumbnailImageUrl: 'https://picsum.photos/seed/3/87' },
-  { latitude: 37.5515, longitude: 126.923, feedCount: 2, thumbnailImageUrl: 'https://picsum.photos/seed/4/87' },
-  { latitude: 37.55, longitude: 126.928, feedCount: 4, thumbnailImageUrl: 'https://picsum.photos/seed/5/87' },
+  {
+    latitude: 37.5507563,
+    longitude: 126.9254901,
+    feedCount: 3,
+    thumbnailImageUrl: 'https://picsum.photos/seed/1/87',
+  },
+  {
+    latitude: 37.552,
+    longitude: 126.927,
+    feedCount: 1,
+    thumbnailImageUrl: 'https://picsum.photos/seed/2/87',
+  },
+  {
+    latitude: 37.5495,
+    longitude: 126.924,
+    feedCount: 5,
+    thumbnailImageUrl: 'https://picsum.photos/seed/3/87',
+  },
+  {
+    latitude: 37.5515,
+    longitude: 126.923,
+    feedCount: 2,
+    thumbnailImageUrl: 'https://picsum.photos/seed/4/87',
+  },
+  {
+    latitude: 37.55,
+    longitude: 126.928,
+    feedCount: 4,
+    thumbnailImageUrl: 'https://picsum.photos/seed/5/87',
+  },
 ];
