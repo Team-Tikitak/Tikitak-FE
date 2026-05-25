22ab246d85fa1cc7408a8f7d5021381632292861
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2026-05-26 04:02:40 +0900

    chore: 마이페이지 외부 링크 업데이트

--- src/pages/myPage/constants/externalLinks.ts
+++ src/pages/myPage/constants/externalLinks.ts
@@ -1,6 +1,6 @@
 export const EXTERNAL_LINKS = {
-  TERMS: 'https://www.notion.so/hyunjin-lee/35da5a553281806b842ecda891104127?source=copy_link',
+  TERMS: 'https://hyunjin-lee.notion.site/35da5a553281806b842ecda891104127?source=copy_link',
   PRIVACY_POLICY:
-    'https://www.notion.so/hyunjin-lee/35da5a553281802d9d38f7d07b92b86a?source=copy_link',
+    'https://hyunjin-lee.notion.site/35da5a553281802d9d38f7d07b92b86a?source=copy_link',
   CUSTOMER_SUPPORT: 'https://open.kakao.com/o/syl9qRvi',
 };
