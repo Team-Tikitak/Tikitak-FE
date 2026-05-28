5afba490a3eedd2cd2b37ee0a4e6c6399edfec8d
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2026-05-28 18:45:34 +0900

    refactor: kakao SDK 정식 타입 정의로 any 제거

--- src/pages/home/hooks/kakao.d.ts
+++ src/pages/home/hooks/kakao.d.ts
@@ -1,6 +1,47 @@
 declare global {
+  interface KakaoLatLng {
+    getLat: () => number;
+    getLng: () => number;
+  }
+
+  interface KakaoPoint {
+    x: number;
+    y: number;
+  }
+
+  interface KakaoProjection {
+    containerPointFromCoords: (latLng: KakaoLatLng) => KakaoPoint;
+  }
+
+  interface KakaoMap {
+    getProjection: () => KakaoProjection;
+  }
+
+  interface KakaoMapOptions {
+    center: KakaoLatLng;
+    level?: number;
+  }
+
+  type KakaoMapEventType = 'drag' | 'idle' | 'click' | 'zoom_changed' | 'center_changed';
+
+  interface KakaoMapEvent {
+    addListener: (target: KakaoMap, type: KakaoMapEventType, handler: () => void) => void;
+    removeListener: (target: KakaoMap, type: KakaoMapEventType, handler: () => void) => void;
+  }
+
+  interface KakaoMapsNamespace {
+    load: (callback: () => void) => void;
+    Map: new (container: HTMLElement, options: KakaoMapOptions) => KakaoMap;
+    LatLng: new (latitude: number, longitude: number) => KakaoLatLng;
+    event: KakaoMapEvent;
+  }
+
+  interface KakaoNamespace {
+    maps: KakaoMapsNamespace;
+  }
+
   interface Window {
-    kakao: any;
+    kakao?: KakaoNamespace;
   }
 }
 
