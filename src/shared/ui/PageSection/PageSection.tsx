b0f0e9673c806e6e724d918cdef549dc1fade6e9
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2026-05-28 18:44:54 +0900

    refactor: shared 컴포넌트 className ref prop 노출

--- src/shared/ui/PageSection/PageSection.tsx
+++ src/shared/ui/PageSection/PageSection.tsx
@@ -1,21 +1,30 @@
+import { type ComponentPropsWithRef, type ReactElement, type ReactNode } from 'react';
 import { cn } from '@/shared/lib';
-import type React from 'react';
 
-interface PageSectionProps {
+interface PageSectionProps extends ComponentPropsWithRef<'section'> {
   title: string;
-  className?: string;
-  icon?: React.ReactElement;
+  icon?: ReactElement;
   iconClick?: () => void;
-  children: React.ReactNode;
+  children: ReactNode;
 }
 
-export const PageSection = ({ title, icon, iconClick, className, children }: PageSectionProps) => (
-  <section className="flex flex-col gap-4">
+export const PageSection = ({
+  title,
+  icon,
+  iconClick,
+  className,
+  children,
+  ref,
+  ...props
+}: PageSectionProps) => (
+  <section ref={ref} className="flex flex-col gap-4" {...props}>
     <div className="flex items-center justify-between">
       <h2 className="body-2 text-black">{title}</h2>
-      <button onClick={iconClick} className="cursor-pointer">
-        {icon}
-      </button>
+      {icon && (
+        <button type="button" onClick={iconClick} className="cursor-pointer">
+          {icon}
+        </button>
+      )}
     </div>
     <div className={cn('flex flex-col', className)}>{children}</div>
   </section>
