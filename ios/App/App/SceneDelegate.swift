59b0c360205e750a8e2d0f5c4cc954822eda08b9
Lee KyeongJoon <rudwns0224@naver.com>
2026-07-04 18:18:26 +0900

    chore: iOS 씬 라이프사이클 추가

---
+++ ios/App/App/SceneDelegate.swift
@@ -0,0 +1,44 @@
+import UIKit
+import Capacitor
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        window?.backgroundColor = .white
+        connectionOptions.urlContexts.forEach(handleOpenURLContext)
+        connectionOptions.userActivities.forEach(handleUserActivity)
+        guard scene is UIWindowScene else { return }
+    }
+
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        URLContexts.forEach(handleOpenURLContext)
+    }
+
+    func scene(_ scene: UIScene, continue userActivity: NSUserActivity) {
+        handleUserActivity(userActivity)
+    }
+
+    private func handleOpenURLContext(_ context: UIOpenURLContext) {
+        var options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+        options[.sourceApplication] = context.options.sourceApplication
+        options[.annotation] = context.options.annotation
+        options[.openInPlace] = context.options.openInPlace
+
+        _ = ApplicationDelegateProxy.shared.application(
+            UIApplication.shared,
+            open: context.url,
+            options: options
+        )
+    }
+
+    private func handleUserActivity(_ userActivity: NSUserActivity) {
+        _ = ApplicationDelegateProxy.shared.application(
+            UIApplication.shared,
+            continue: userActivity,
+            restorationHandler: { _ in }
+        )
+    }
+
+}
